7b259bee35550404efcaf9716b829dc1dcb79610
dannitay <dannielletaylor@outlook.com>
2020-01-13 22:16:29 -0600

    updating portfolio with Crystal Collector Game

---
+++ javascript/portlink.js
@@ -0,0 +1,8 @@
+$('#psGame').on('click', function(){
+    window.open("https://d-taylor6403.github.io/Psychic-Game/", "_blank");
+})
+
+$('#ccGame').on('click', function(){
+    window.open("https://d-taylor6403.github.io/unit-4-game/", "_blank");
+})
+
